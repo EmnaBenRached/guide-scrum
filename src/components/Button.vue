2af2a174c1a3552916fcfe7732eaf753a7ededb5
Emna Ben Rached <ember132@ulaval.ca>
2024-05-29 14:41:22 -0400

    Ajout de storybook

---
+++ src/components/Button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button
+        type="button"
+        class="bg-orange-500 p-2"
+        @click="onClick"
+        :style="style"
+    >
+        {{ label }}
+    </button>
+</template>
+
+<script setup lang="ts"></script>
